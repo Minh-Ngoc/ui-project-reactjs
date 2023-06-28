d14531199397401fcc121beeba804298a3afca03
Minh-Ngoc <ngocminh101100@gmail.com>
2023-06-28 09:06:41 +0700

    create login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,15 +6,45 @@ import github from '../../assets/imgs/Github.png';
 import facebook from '../../assets/imgs/Facebook.png';
 import Button from '../../components/Button';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+// React Final Form
+import { Form, Field } from 'react-final-form'
+import { FORM_ERROR } from 'final-form'
+// React toastify
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
 
 const cx = classNames.bind(styles);
 
 const Login = () => {
+    const navigate = useNavigate();
+    
     const itemLogin = [google, github, facebook];
     const [type, setType] = useState('password');
-    const handleShowPassword = (e) => type === 'password' ? setType('text') : setType('password')
+
+    const handleShowPassword = (e) => type === 'password' ? setType('text') : setType('password');
+    
+    const onSubmit = async values => {
+        if (values.username !== 'ngocminh') {
+          return { username: 'Unknown username' }
+        }
+        if (values.password !== 'ngocminh') {
+          return { [FORM_ERROR]: 'Login Failed' }
+        }
+        toast.success("Login successfully!", {
+            position: toast.POSITION.TOP_RIGHT,
+            style: {
+                minWidth: '400px',
+                padding: '1rem',
+                right: '4.2rem'
+            }
+        })
+        setTimeout(() => navigate('/'), 2000)
+    }
+
     return (
         <div className={cx('wrapper')}>
+            <ToastContainer autoClose={1500} />
             <div className={cx('container')}>
                 <div id={cx('login')}>
                     <div className={cx('login__logo')}>
@@ -28,27 +58,79 @@ const Login = () => {
                     <h1>Log In</h1>
 
                     <div className={cx('login__form')}>
-                        <form>
-                            <div className={cx('form__group')}>
-                                <label htmlFor='username'>Username</label>
-                                <input id='username' type='text' placeholder='username'/>
-                            </div>
-                            <div className={cx('form__group', 'form__password')}>
-                                <div className={cx('form__label__group')}>
-                                    <label htmlFor='password'>Password</label>
-                                    <p className={cx('forgot__password')}>Forgot Password ?</p>
-                                </div>
-                                <span onClick={handleShowPassword} className={cx('hide__password')}></span>
-                                <input id='password' type={type} placeholder='password'/>
-                            </div>
-                            
-                            <div className={cx('form__group', 'isValid')}></div>
 
-                            <Button 
-                                className={cx('login__btn')}
-                                textContent='Login'
-                            />
-                        </form>
+                    <Form
+                        onSubmit={onSubmit}
+                        validate={values => {
+                            const errors = {}
+                            
+                            if (!values.username) {
+                                errors.username = 'Username không được bỏ trống!'
+                            }
+                            if (!values.password) {
+                                errors.password = 'Password không được bỏ trống!'
+                            } 
+                            if(values.username && values.password && values.username !== 'ngocminh') {
+                                errors.username = 'Username không tồn tại!'
+                            }
+                            return errors
+                        }}
+                        render={({
+                            submitError,
+                            handleSubmit,
+                            form,
+                            submitting,
+                            pristine,
+                            values
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <Field name="username">
+                                    {({ input, meta }) => (
+                                        <div className={cx('form__group')}>
+                                            <label htmlFor='username'>Username</label>
+                                            <input 
+                                                {...input}
+                                                type='text' 
+                                                placeholder='Enter: ngocminh'
+                                                className={cx((meta.error || meta.submitError) && meta.touched && 'ipError')}
+                                            />
+                                            {(meta.error || meta.submitError) && meta.touched && (
+                                                <span className={cx('errorMessage')}>{meta.error || meta.submitError}</span>
+                                            )}
+                                        </div>
+                                    )}
+                                </Field>
+                                <Field name="password">
+                                    {({ input, meta }) => (
+                                        <>
+                                            <div className={cx('form__group', 'form__password')}>
+                                                <div className={cx('form__label__group')}>
+                                                    <label htmlFor='password'>Password</label>
+                                                    <p className={cx('forgot__password')}>Forgot Password ?</p>
+                                                </div>
+                                                <span onClick={handleShowPassword} className={cx('hide__password')}></span>
+                                                <input 
+                                                    {...input}
+                                                    type={type} 
+                                                    placeholder='Enter: ngocminh'
+                                                    className={cx((meta.error || meta.submitError) && meta.touched && 'ipError')}
+                                                />
+                                            </div>
+                                            {meta.error && meta.touched && <span className={cx('errorMessage')}>{meta.error}</span>}
+                                        </>
+                                    )}
+                                </Field>
+                                
+                                {submitError && <div className="error">{submitError}</div>}
+
+                                <Button 
+                                    className={cx('login__btn')}
+                                    textContent='Login'
+                                    type="submit"
+                                />
+                            </form>
+                        )}
+                    />
                     </div>
                     <div className={cx('login__with')}>
                         <p>or continue with</p>
